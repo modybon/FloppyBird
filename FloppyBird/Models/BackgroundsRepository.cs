2f3bc867db087202725a9e564579dbf429e3c28e
modybon <61365506+modybon@users.noreply.github.com>
2022-04-20 14:19:52 -0400

    Added skins and background repositories

---
+++ FloppyBird/Models/BackgroundsRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FloppyBird.Models
+{
+    public class BackgroundsRepository
+    {
+        public ImageSource DefaultBackground = ImageSource.FromResource("FloppyBird.Assets.Images.Skins.pokeball.png");
+        private List<ImageSource> _backgroundsList = new List<ImageSource>();
+        public List<ImageSource> BackgroundsList
+        {
+            get
+            {
+                return _backgroundsList;
+            }
+        }
+
+        public BackgroundsRepository()
+        {
+        }
+
+        //public void PopulateBackgrounds()
+        //{
+
+        //}
+
+        public void AddBackgrounds()
+        {
+
+        }
+
+
+    }
+}
---
+++ FloppyBird/Models/SkinsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FloppyBird.Models
+{
+    public class SkinsRepository
+    {
+        public ImageSource DefaultSkin = ImageSource.FromResource("FloppyBird.Assets.Images.Skins.pokeball.png");
+        private List<ImageSource> _skinsList = new List<ImageSource>();
+        public List<ImageSource> SkinsList
+        {
+            get
+            {
+                return _skinsList;
+            }
+        }
+        public SkinsRepository()
+        {
+        }
+
+        //public void PopulateSkins()
+        //{
+
+        //}
+
+        public void AddSkin()
+        {
+
+        }
+    }
+}
